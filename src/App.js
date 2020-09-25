f99f227e52f1bb871771e12ff1f7f278a1a935c1
Simone Hoffmann <simohoff@gmx.de>
2020-09-25 23:19:14 +0200

    well

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./images/logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Simone is a Software Developer based in Mannheim currently working at
-          Pathmate Technologies
+      <header className="App-header" style={{ backgroundColor: "#111111" }}>
+        <p>Simone</p>
+        <p>is a Software Developer based in </p>
+        <p>Mannheim </p>
+        <p>currently working at</p>
+        <p>Pathmate Technologies</p>
+
+        <p>.</p>
+        <p className="header-subtitle">
+          Focused on product and visual design , passionate about gaming,
+          technology and dogs
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      </header>
+      <body>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </body>
     </div>
   );
 }

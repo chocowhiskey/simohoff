61c96c2ff4768dbe968e1d1569539233bcab7693
Simone Hoffmann <simohoff@gmx.de>
2020-09-25 22:55:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          Simone is a Software Developer based in Mannheim currently working at
+          Pathmate Technologies
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

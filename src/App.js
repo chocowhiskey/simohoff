a07503f319a2e078a0c0a1388f6cb05f7c13f2eb
Simone Hoffmann <simohoff@gmx.de>
2020-09-26 11:45:52 +0200

    ïntroduction sentence with lil styling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 import React from "react";
 import logo from "./images/logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" style={{ backgroundColor: "#111111" }}>
-        <p>Simone</p>
-        <p>is a Software Developer based in </p>
-        <p>Mannheim </p>
-        <p>currently working at</p>
-        <p>Pathmate Technologies</p>
+        <div className="introduction-two-sentences">
+          <p className="name">Simone</p>
+          <p>is a Software Developer based in </p>
+          <p className="location">Mannheim </p>
+          <p>currently working at</p>
+          <p className="workplace">Pathmate Technologies</p>
 
-        <p>.</p>
-        <p className="header-subtitle">
-          Focused on product and visual design , passionate about gaming,
-          technology and dogs
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p>.</p>
+          <p className="header-subtitle">
+            Focused on product and visual design , passionate about gaming,
+            technology and dogs
+          </p>
+        </div>
       </header>
-      <body>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </body>
     </div>
   );
 }

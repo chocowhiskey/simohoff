d1da911a0a1547f9fb16cbcd6cff561b43e2998f
Simone Hoffmann <simohoff@gmx.de>
2020-09-26 14:41:18 +0200

    sexy hover effects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from "react";
-import logo from "./images/logo.svg";
+import simone from "./images/simone.jpeg";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" style={{ backgroundColor: "#111111" }}>
-        <div className="introduction-two-sentences">
-          <p className="name">Simone</p>
-          <p>is a Software Developer based in </p>
-          <p className="location">Mannheim </p>
-          <p>currently working at</p>
-          <p className="workplace">Pathmate Technologies</p>
+        <div style={{ display: "flex" }}>
+          <div className="introduction-two-sentences">
+            <p id="name">Simone</p>
+            <p>is a Software Developer based in </p>
+            <p className="location">Mannheim </p>
+            <p>currently working at</p>
+            <p className="workplace">Pathmate Technologies</p>
 
-          <p>.</p>
-          <p className="header-subtitle">
-            Focused on product and visual design , passionate about gaming,
-            technology and dogs
-          </p>
+            <p>.</p>
+            <p className="introduction-subtitle">
+              Focused on product and visual design , passionate about gaming,
+              technology and dogs
+            </p>
+            <img
+              id="simone"
+              src={simone}
+              alt="Simone Hoffmann"
+              style={{ alignContent: "center" }}
+            />
+          </div>
         </div>
       </header>
     </div>
